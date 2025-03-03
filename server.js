ec3e0aac3bd906f5821257a0a40f8368f2f6df9f
Wesky Dorcenna <dorcennaflashwesky@gmail.com>
2025-03-03 12:52:36 -0500

    second commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const officegen = require('officegen');
 const officeparser = require('officeparser');
 
 const app = express();
-const port = 3004;
+const port = 3000;
 const upload = multer({ dest: 'uploads/' });
 
 app.use(express.json());
